b5777f9ea32e3c97da3d9621a703308ab49e9bcc
yanvr <yanvrublevicius@gmail.com>
2021-10-18 17:15:43 -0300

    adicionado exercícios modulo 3 Lets Code

---
+++ exercicio1.js
@@ -0,0 +1,38 @@
+class Person {
+  constructor(fistName, lastName, age, gender, [...interests]) {
+    this.fistName = fistName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+    this.interests = [...interests];
+  }
+  bio() {
+    return `${this.fistName} ${this.lastName} is ${
+      this.age
+    } years old. They like ${this.interests.join(", ")}`;
+  }
+  gretting() {
+    return `Hi! I'm ${this.fistName}`;
+  }
+}
+
+class Teacher extends Person {
+  constructor(firstName, lastName, age, gender, [...interests], subject) {
+    super(firstName, lastName, age, gender, [...interests]), subject;
+    this.subject = subject;
+  }
+
+  gretting() {
+    return `Hello my name is ${this.fistName} ${this.lastName}, and I teach ${this.subject}`;
+  }
+}
+
+class Student extends Person {
+  constructor(fistName, lastName, age, gender, [...interests]) {
+    super(fistName, lastName, age, gender, [...interests]);
+  }
+
+  gretting() {
+    `Yo! I'm ${this.firstName}`;
+  }
+}
---
+++ exercicio2.js
@@ -0,0 +1,101 @@
+class BookLists {
+  constructor() {
+    this.allBooks = [];
+    this.numberOfReadBooks;
+    this.numberOfUnreadBooks;
+    this.nextBookToRead;
+    this.currentBookToRead;
+    this.lastBookToRead;
+  }
+
+  listBooks() {
+    return this.allBooks;
+  }
+
+  get numberOfReadBooks() {
+    return this.allBooks.filter((book) => book.hasRead).length;
+  }
+
+  get numberOfUnreadBooks() {
+    return this.allBooks.filter((book) => !book.hasRead).length;
+  }
+
+  readBooks() {
+    const readBooks = this.allBooks.filter((book) => book.hasRead);
+    return readBooks;
+  }
+
+  unreadBooks() {
+    const unreadBooks = this.allBooks.filter((book) => !book.hasRead);
+    return unreadBooks;
+  }
+
+  setCurrentBookToRead(title) {
+    if (this.currentBookToRead != undefined) {
+      return "You already reading a book, try to finish to start other";
+    }
+    for (const book of this.allBooks) {
+      if (book.title === title) {
+        this.currentBookToRead = book;
+        return;
+      }
+    }
+    return "The Book was not found";
+  }
+
+  finishCurrentBook() {
+    if (this.currentBookToRead === undefined) {
+      return "You don't have a current book";
+    }
+    for (const book of this.allBooks) {
+      if (book === this.currentBookToRead) {
+        book.hasRead = true;
+        this.lastBookToRead = book;
+        this.currentBookToRead = undefined;
+        this.nextBookToRead = this.allBooks.find((book) => !book.hasRead);
+      }
+    }
+  }
+
+  addBook(title, genre, author, hasRead) {
+    const newBook = new Book(title, genre, author, hasRead);
+    if (this.allBooks.includes(newBook)) {
+      return "The Book already added, try add other book";
+    }
+    this.allBooks = [...this.allBooks, newBook];
+  }
+
+  getBookBy(bookAtribute, userEntries) {
+    const entriesToUpper = userEntries.toUpperCase();
+    const filterBooks = this.allBooks.filter(
+      (book) => book[bookAtribute].toUpperCase() === entriesToUpper
+    );
+    return filterBooks.length > 0
+      ? filterBooks
+      : `No book by this ${bookAtribute} was found`;
+  }
+
+  getReadingSuggestions() {
+    if (this.nextBookToRead === undefined) {
+      return "We don't have none suggestions for you, try again later";
+    }
+    return `We find a new book for you read, 
+            how about reading ${this.nextBookToRead}`;
+  }
+}
+
+class Book {
+  constructor(title, genre, author, hasRead) {
+    this.title = title || "Unknown title";
+    this.genre = genre || "Unknown genre";
+    this.author = author || "Unknown author";
+    this.hasRead = hasRead || false;
+    this.readDate = this.hasRead ? new Date(Date.now()) : "No read date";
+  }
+}
+
+let book = new BookLists();
+book.addBook("O livro muito loco", "terror", "Ygor", true);
+book.addBook("Meu novo mundo", "terror", "Ygor", true);
+
+console.log(book.getBookBy("genre", "terror"));
